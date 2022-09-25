50342092d52db0cd0d9b388236d90b9583078612
Hatala Mark <hatala.mark@gmail.com>
2022-09-25 18:42:54 +0200

    update

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import CV from '../../pics/Hatala Márk_CV_hun.pdf'
 
 export default function CTA() {
     return (
         <div className='cta'>
-            <a href='' download className='btn'>CV Letöltése</a>
+            <a href={CV} download className='btn'>CV Letöltése</a>
             <a href='#contact' className='btn btn-primary'>Írj üzenetet!</a>
         </div>
     )
